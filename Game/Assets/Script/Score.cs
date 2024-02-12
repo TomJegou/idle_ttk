f10c5bf356f3170c588628907fa64b2feae36800
TomJegou <tomjegou10@gmail.com>
2024-02-12 10:54:51 +0100

    wip

--- Game/Assets/Script/Score.cs
+++ Game/Assets/Script/Score.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    public TextMeshProUGUI textMeshPro;
     public 
     // Start is called before the first frame update
     void Start()
     {
-        
+        textMeshPro.color = Color.green;
+        textMeshPro.text = "fdsjfdsklfjds";
     }
 
     // Update is called once per frame
