2a37c60ca04c8a6ab3d91657235e5e7300cfe8a0
TomJegou <tomjegou10@gmail.com>
2024-02-19 10:11:22 +0100

    change public to private score

--- Game/Assets/Script/MainGame.cs
+++ Game/Assets/Script/MainGame.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class MainGame : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
-    public int score = 0;
+    private int score = 0;
     private string formatStringScore = "Your score: {0}";
     // Start is called before the first frame update
     void Start()
